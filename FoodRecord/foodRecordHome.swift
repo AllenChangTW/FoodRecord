b81949c6192a68e390bc5be6cc20dc512fcbe0f0
allenchang <iamallenchang@gmail.com>
2016-12-21 11:34:45 +0800

    test

---
+++ FoodRecord/foodRecordHome.swift
@@ -0,0 +1,24 @@
+//
+//  foodRecordHome.swift
+//  FoodRecord
+//
+//  Created by AllenChang on 2016/12/21.
+//  Copyright © 2016年 AllenChang. All rights reserved.
+//
+
+import UIKit
+
+class foodRecordHome: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
